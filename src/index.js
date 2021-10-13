dc1b545265bcdadf82fb15328b558946c666d0e1
Azaray <leonardoroelofse@gmail.com>
2021-10-13 19:17:54 +0100

    Added divider validation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { arraySplitter } from "./utils/arraySplitter.js";
 
 const array = [4, 5, 7, 3, 2];
-const divider = 3;
+const divider = 0;
 console.log("Array used is " + array);
 console.log("Divider used is " + divider);
 
-console.log(arraySplitter(array, divider));
+try {
+    console.log(arraySplitter(array, divider));
+} catch (error) {
+    console.error(error);
+}
--- src/utils/arraySplitter.js
+++ src/utils/arraySplitter.js
@@ -6,6 +6,9 @@
  * @returns {Array[]}
  */
 function  arraySplitter(array, divider) {
+    if (divider === 0 || !Number.isInteger(divider)) {
+        throw 'Divider is not an integer!'
+    }
     const newArray = []
     while (array.length != 0) {
         var splicedArray = array.splice(0,divider);
